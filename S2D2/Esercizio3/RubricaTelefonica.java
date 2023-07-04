bc71e0f5359935cf17075cfc52a82a902edbbc6c
Luca Quaglino <quagli.luca@gmail.com>
2023-07-04 16:18:35 +0200

    update2

---
+++ S2D2/Esercizio3/RubricaTelefonica.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RubricaTelefonica {
+
+	private Map<String, String> contatti;
+
+	public RubricaTelefonica() {
+		contatti = new HashMap<>();
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
